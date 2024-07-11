564e418026f6f6815c32888674fe6cbfa01c254f
Dhvanit <monparadhvanit@gmail.com>
2024-07-11 19:41:17 +0530

    basic routing setup

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 
+const app = express()
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -12,8 +14,10 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+// routes import
+import userRouter from "./routes/user.routes.js"
 
-
-const app = express()
+// routes declaration
+app.use("/api/v1/users", userRouter)
 
 export { app }
