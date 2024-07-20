78d99dc1e3ebd1b8a440763af60299a4b6e560f9
Dhvanit <monparadhvanit@gmail.com>
2024-07-20 20:35:58 +0530

    dashboard update

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,16 +5,113 @@ import { Like } from "../models/like.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { User } from "../models/user.model.js"
 
 const getChannelStats = asyncHandler(async (req, res) => {
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    // Get the channel stats like total video views, total subscribers, total videos, total likes etc.
 
+    const channelId = req.user._id
 
+    if (!channelId) {
+        throw new ApiError(400, "Channel ID is required")
+    }
+
+    const channelStats = await User.aggregate([
+        {
+            $match: {
+                _id: channelId
+            }
+        },
+        {
+            $lookup: {
+                from: "video",
+                localField: "_id",
+                foreignField: "owner",
+                as: "videos"
+            }
+        },
+        {
+            $lookup: {
+                from: "subscription",
+                localField: "_id",
+                foreignField: "channel",
+                as: "subscribers"
+            }
+        },
+        {
+            $lookup: {
+                from: "like",
+                localField: "_id",
+                foreignField: "likedBy",
+                as: "likes"
+            }
+        },
+        {
+            $addFields: {
+                videosCount: {
+                    $size: "$videos"
+                },
+                subscribersCount: {
+                    $size: "$subscribers"
+                },
+                likesCount: {
+                    $size: "$likes"
+                }
+            }
+        }
+    ]);
+
+    if (channelStats.length === 0) {
+        throw new ApiError(404, "No channel found")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            channelStats,
+            "Channel stats fetched successfully"
+        ))
 
 })
 
 const getChannelVideos = asyncHandler(async (req, res) => {
-    // TODO: Get all the videos uploaded by the channel
+    // Get all the videos uploaded by the channel
+    const { channelId } = req.params; // because channel is also a user so we can perform task by channelId
+
+    if(!channelId) {
+        throw new ApiError(400, "Channel ID is required")
+    }
+
+    console.log(channelId)
+
+    const videos = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "_id",
+                foreignField: "owner",
+                as: "videos"
+            }
+        }
+    ])
+
+    if (videos.length === 0 || !videos[0].videos) {
+        throw new ApiError(404, "No videos found for this channel")
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            videos[0].videos,
+            "Channel videos fetched successfully"
+        ))
 
 })
 
